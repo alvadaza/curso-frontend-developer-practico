ec7987639d07c8328661217bc8ef8a3711d8f461
github_username <am.company.edu@gmail.com>
2023-08-09 22:26:22 -0500

    lista de productos

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMunu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDestopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -36,3 +37,83 @@ function toggleCarritoAside() {
     }
     aside.classList.toggle('inactive');
 }
+
+const produclist = [];
+produclist.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+produclist.push({
+    name: 'pantalla',
+    price: 320,
+    image: "https://images.pexels.com/photos/326512/pexels-photo-326512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+produclist.push({
+    name: 'portatil',
+    price: 620,
+    image: "https://images.pexels.com/photos/6446709/pexels-photo-6446709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+produclist.push({
+    name: 'mause',
+    price: 30,
+    image: "https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+produclist.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+produclist.push({
+    name: 'pantalla',
+    price: 320,
+    image: "https://images.pexels.com/photos/326512/pexels-photo-326512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+produclist.push({
+    name: 'portatil',
+    price: 620,
+    image: "https://images.pexels.com/photos/6446709/pexels-photo-6446709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+produclist.push({
+    name: 'mause',
+    price: 30,
+    image: "https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+function renderProducts() {
+    for (product of produclist) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        /* product= {name, price, image} -> product.image*/
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.append(productPrice, productName)
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', "./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.append(productImgCard)
+    
+        productInfo.append(productInfoDiv, productInfoFigure)
+    
+        productCard.append(productImg, productInfo)
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(produclist);
